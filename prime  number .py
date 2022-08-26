d55667b59bcda60c9f82022d7c992633861793d6
POOJABINJWA <110899989+POOJABINJWA@users.noreply.github.com>
2022-08-26 12:56:41 +0530

    Update prime  number .py

--- prime  number .py
+++ prime  number .py
@@ -1,14 +1,14 @@
-# a=int(input("enter the number :"))
-# c=0
-# i=1
-# while i<=a:
-#     if a%i==0:
-#         c=c+1
-#     i=i+1
-# if c==2:
-#     print("prime")
-# else:
-#     print("not")            
+a=int(input("enter the number :"))
+c=0
+i=1
+while i<=a:
+    if a%i==0:
+        c=c+1
+    i=i+1
+if c==2:
+    print("prime")
+else:
+    print("not")            
 
 
 i=1
